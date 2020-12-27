97a6066b0345e209ce3c1ed8d71ec314ae53512a
ShaurHerng <63914779+ShaurHerng@users.noreply.github.com>
2020-12-27 15:49:18 +0800

    changes

--- checkWin.cpp
+++ checkWin.cpp
@@ -8,7 +8,26 @@ int checkWin(int m_arr[][5])
     int temp = 0;
     int tempH = 0;
     int tempW = 0;
+    int len;
+    int m_winLen = 3;
+
+    for (tempH = 0; tempH < m_height; ++tempH)
+    {
+        int currentIndex = 0;
+
+        for (tempW = 0; tempW < m_width; ++tempW)
+        {
+            len = 0;
+
+            if ()
+        }
+    }
+
+
 }
+
+// logic for any sized boards??
+// I should fix this first actually lmaoo
 
 int main()
 {
